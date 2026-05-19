4a8e419c20c1db61949e59fea5a5c2c5d2b62e46
Elton <javawocky@gmail.com>
2026-05-19 17:28:16 +1200

    Use api-turkeymxbingo.thefryup.com for prod backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://motobingo-api-prod.javawocky.workers.dev/api/v1',
+  apiUrl: 'https://api-turkeymxbingo.thefryup.com/api/v1',
   auth0: {
     domain: 'dev-xlhmy2q3ti2zo0ad.us.auth0.com',
     clientId: 'sgbZkzh0cgUxFTaabQjAZw8WGl371yaC',
